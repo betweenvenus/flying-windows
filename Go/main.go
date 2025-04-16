d01fbf153f38fd08ead2cd37d1a3a0364c914d9f
Jon Highsmith <jon@gojira.dev>
2025-04-16 16:06:00 -0400

    omg red

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"honnef.co/go/js/dom/v2"
+)
 
 func main() {
 	fmt.Println("hello world")
+	document := dom.GetWindow().Document()
+	body := document.QuerySelector("body")
+	h1 := document.CreateElement("h1")
+	h1.SetInnerHTML("hello world")
+	body.AppendChild(h1)
+	body.SetAttribute("style", "background-color: red;")
 }
